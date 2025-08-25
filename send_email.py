7cf713d63b7bf1183b60892ac96194604a16c26a
mkih76 <142023477+mkih76@users.noreply.github.com>
2025-08-25 18:18:05 +0800

    Create send_email.py

---
+++ send_email.py
@@ -0,0 +1,13 @@
+import smtplib
+from email.mime.text import MIMEText
+from config import EMAIL_ADDRESS, EMAIL_PASSWORD, RECIPIENT_EMAIL
+
+def send_email(subject, body):
+    msg = MIMEText(body, "plain", "utf-8")
+    msg["Subject"] = subject
+    msg["From"] = EMAIL_ADDRESS
+    msg["To"] = RECIPIENT_EMAIL
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        server.send_message(msg)
